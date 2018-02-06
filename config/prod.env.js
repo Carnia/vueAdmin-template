b166d0b9ed7f2fd0e7b255df77e03ffba391782b
xlqdys <xlqdys@foxmail.com>
2018-02-06 11:15:28 +0800

    first commit

---
+++ config/prod.env.js
@@ -0,0 +1,6 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+  BASE_API: '" https://easy-mock.com/mock/5a653ea3fc1b280416439b0a/MEC"'
+};
