01c1090cb662e45946af6762cf532797c898087b
xlqdys <xlqdys@foxmail.com>
2018-02-08 18:01:03 +0800

    面板1

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="dashboard-container">
-    <div v-text="list"></div>
-    <el-row :gutter="10">
+    <el-row :gutter="20">
       <el-col :xs="24" :sm="24" :md="22" :lg="12" :xl="12">
         <div class="dashboard-block">
           <div class="head">病历总数：</div>
@@ -10,20 +9,20 @@
             <el-row>
               <el-col :span="8">
                 <el-row>
-                  <el-col :span="8">昨日新增：</el-col>
-                  <el-col :span="16" v-text="list.dayMedicallCount"></el-col>
+                  <el-col :span="10">昨日新增：</el-col>
+                  <el-col :span="12" v-text="list.dayMedicallCount"></el-col>
                 </el-row>
               </el-col>
               <el-col :span="8">
                 <el-row>
-                  <el-col :span="8">近7天新增：</el-col>
-                  <el-col :span="16" v-text="list.weekMedicallCount"></el-col>
+                  <el-col :span="10">近7天新增：</el-col>
+                  <el-col :span="12" v-text="list.weekMedicallCount"></el-col>
                 </el-row>
               </el-col>
               <el-col :span="8">
                 <el-row>
-                  <el-col :span="8">近30天新增：</el-col>
-                  <el-col :span="16" v-text="list.monthMedicallCount"></el-col>
+                  <el-col :span="10">近30天新增：</el-col>
+                  <el-col :span="12" v-text="list.monthMedicallCount"></el-col>
                 </el-row>
               </el-col>
               <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
@@ -32,19 +31,104 @@
           </div>
         </div>
       </el-col>
+      
       <el-col :xs="24" :sm="24" :md="22" :lg="12" :xl="12">
         <div class="dashboard-block">
           <div class="head">协诊总数：</div>
           <div class="num"><span v-text="list.allConsCount"></span> 例</div>
+          <div class="infoExtra">
+            其中转诊数：<span v-text="list.allTsConsCount"></span>
+            手术数： <span v-text="list.allOpsCount"></span>
+          </div>
           <div class="info">
             <el-row>
-              <el-col :span="8"><div class="grid-content bg-purple">昨日新增</div></el-col>
-              <el-col :span="8"><div class="grid-content bg-purple-light">近7天新增</div></el-col>
-              <el-col :span="8"><div class="grid-content bg-purple">近30天新增</div></el-col>
+              <el-col :span="8">
+                <el-row>
+                  <el-col :span="10">昨日新增：</el-col>
+                  <el-col :span="12" v-text="list.dayConsCount"></el-col>
+                </el-row>
+              </el-col>
+              <el-col :span="8">
+                <el-row>
+                  <el-col :span="10">近7天新增：</el-col>
+                  <el-col :span="12" v-text="list.weekConsCount"></el-col>
+                </el-row>
+              </el-col>
+              <el-col :span="8">
+                <el-row>
+                  <el-col :span="10">近30天新增：</el-col>
+                  <el-col :span="12" v-text="list.monthConsCount"></el-col>
+                </el-row>
+              </el-col>
+              <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
+              <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
             </el-row>
           </div>
         </div>
       </el-col>
+      
+      <el-col :xs="24" :sm="24" :md="22" :lg="12" :xl="12">
+        <div class="dashboard-block">
+          <div class="head">医院总数：</div>
+          <div class="num"><span v-text="list.allHospCount"></span> 所</div>
+          <div class="info">
+            <el-row>
+              <el-col :span="8">
+                <el-row>
+                  <el-col :span="10">昨日新增：</el-col>
+                  <el-col :span="12" v-text="list.dayHospCount"></el-col>
+                </el-row>
+              </el-col>
+              <el-col :span="8">
+                <el-row>
+                  <el-col :span="10">近7天新增：</el-col>
+                  <el-col :span="12" v-text="list.weekHospCount"></el-col>
+                </el-row>
+              </el-col>
+              <el-col :span="8">
+                <el-row>
+                  <el-col :span="10">近30天新增：</el-col>
+                  <el-col :span="12" v-text="list.monthHospCount"></el-col>
+                </el-row>
+              </el-col>
+              <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
+              <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+            </el-row>
+          </div>
+        </div>
+      </el-col>
+
+      <el-col :xs="24" :sm="24" :md="22" :lg="12" :xl="12">
+        <div class="dashboard-block">
+          <div class="head">医生总数：</div>
+          <div class="num"><span v-text="list.allDoctorCount"></span> 所</div>
+          <div class="info">
+            <el-row>
+              <el-col :span="8">
+                <el-row>
+                  <el-col :span="10">昨日新增：</el-col>
+                  <el-col :span="12" v-text="list.dayDoctorCount"></el-col>
+                </el-row>
+              </el-col>
+              <el-col :span="8">
+                <el-row>
+                  <el-col :span="10">近7天新增：</el-col>
+                  <el-col :span="12" v-text="list.weekDoctorCount"></el-col>
+                </el-row>
+              </el-col>
+              <el-col :span="8">
+                <el-row>
+                  <el-col :span="10">近30天新增：</el-col>
+                  <el-col :span="12" v-text="list.monthDoctorCount"></el-col>
+                </el-row>
+              </el-col>
+              <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
+              <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+            </el-row>
+          </div>
+        </div>
+      </el-col>
+
     </el-row>
     
   </div>
@@ -62,7 +146,24 @@ export default {
         dayMedicallCount: '...',
         weekMedicallCount: '...',
         monthMedicallCount: '...',
-        allConsCount: '...'
+
+        allConsCount: '...',
+        dayConsCount: '...',
+        weekConsCount: '...',
+        monthConsCount: '...',
+
+        allHospCount: '...',
+        dayHospCount: '...',
+        weekHospCount: '...',
+        monthHospCount: '...',
+
+        allDoctorCount: '...',
+        dayDoctorCount: '...',
+        weekDoctorCount: '...',
+        monthDoctorCount: '...',
+
+        allTsConsCount: '...',
+        allOpsCount: '...'
       }
     }
   },
@@ -102,6 +203,7 @@ export default {
   &-block {
     padding: 20px;
     background-color: #ffffff;
+    margin: 10px 0;
     color:$grey;
     border-radius: 10px;
     box-shadow: #000 0 0 10px 0;
@@ -124,12 +226,24 @@ export default {
         line-height: 40px;
       }
     }
+    .infoExtra{
+      margin-left: 30px;
+      height: 40px;
+      line-height: 40px;
+    }
+    .infoExtra+.info{
+      margin-top: 0;
+    }
     .info{
       height: 30px;
       margin: 40px 0 0 20px;
       line-height: 30px;
       color: #a6a6a6;
       position: relative;
+      .el-col-12{
+        font-size: 18px;
+        color: #000000;
+      }
     }
   }
 }
